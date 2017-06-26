5c53fe175489a5f328580220733040fa8fd278cc
Valerio Belli <belli.valerio@gmail.com>
2017-06-26 23:55:50 +0200

    -- First commit

---
+++ src/routes.php
@@ -0,0 +1,4 @@
+<?php
+// Routes
+
+require_once __DIR__ . '/routes/index.php';
---
+++ src/routes/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$app->get('/[{name}]', function ($request, $response, $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
