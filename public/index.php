6966cebc344a8a90dc3efa504fc30c5c04bc591c
Valerio Belli <belli.valerio@gmail.com>
2017-07-14 07:54:49 +0200

    Update the index.php handler.

--- public/index.php
+++ public/index.php
@@ -5,18 +5,21 @@ require __DIR__ . '/../vendor/autoload.php';
 $target = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
 
-if (preg_match('/\/(handler){1}/', $target) && $method === 'POST') {
-    return include __DIR__.'/../app/controller/handler.php';
-} else if (preg_match('/\/(validate){1}/', $target) && $method === 'GET') {
-    return include __DIR__.'/../app/controller/handler.php';
-} else if (preg_match('/\/(hashes){1}\/[a-zA-Z0-9]+\.(zip){1}/', $target)
-    && $method === 'GET') {
-    $file = preg_split('/\//', $target)[2];
-    if (file_exists(__DIR__.'/hashes/'.$file)) {
-        return __DIR__.'/hashes/'.$file;
+if ($method === 'POST') {
+    if (preg_match('/\/(handler){1}/', $target) && $method === 'POST') {
+        return include __DIR__.'/../app/controller/handler.php';
     }
-} else if (preg_match('/\//', $target) && $method === 'GET') {
-    return require __DIR__.'/../app/view/index.html';
+} else if ($method === 'GET') {
+    if (preg_match('/\/(validate){1}/', $target)) {
+        return include __DIR__.'/../app/controller/handler.php';
+    } else if (preg_match('/\/(hashes){1}\/[a-zA-Z0-9]+\.(zip){1}/', $target)) {
+        $file = preg_split('/\//', $target)[2];
+        if (file_exists(__DIR__.'/hashes/'.$file)) {
+            return __DIR__.'/hashes/'.$file;
+        }
+    } else if (preg_match('/\//', $target)) {
+        return require __DIR__.'/../app/view/index.html';
+    }
 }
 
 return require_once '../app/view/404.html';
